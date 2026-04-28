fb013f0faf47448490898cc1c39cd2a7811c07f8
Analine Francino Bastos <analinebastos@mtprev.local>
2026-04-28 17:20:57 -0400

    Adiciona botao para baixar comprovantes

--- app_premium_reforma.py
+++ app_premium_reforma.py
@@ -508,7 +508,7 @@ else:
                 )
         except:
             st.warning("Arquivo não encontrado.")
-elif menu == "Todos os gastos":
+ elif menu == "Todos os gastos":
     st.header("📄 Todos os gastos cadastrados")
 
     df = pd.read_sql_query("SELECT * FROM gastos ORDER BY data DESC", conexao)
